bcd9cf1a0db08e25e8653ce51d3ac757844cf5ce
Viraj Navkal <vnavkal0@gmail.com>
2021-01-12 15:36:33 -0800

    simple pythonnet dockerfile

---
+++ Example.cs
@@ -0,0 +1,27 @@
+using Azure.Storage.Blobs; //used for step 2
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace LinuxEnvironmentTest
+{
+    public class TestClass
+    {
+        public bool Method()
+        {
+            /* step 1 */
+            Console.WriteLine(".Net standard 2.0");
+            JArray array = new JArray();
+            array.Add("Manual text");
+            array.Add(new DateTime(2000, 5, 23));
+            JObject o = new JObject();
+            o["MyArray"] = array;
+            string json = o.ToString();
+            Console.WriteLine(json);
+
+            /* step 2 */
+            BlobContainerClient container = new BlobContainerClient("****connectionstring****", "code");
+            Console.WriteLine(container.Exists());
+            return true;
+        }
+    }
+}
